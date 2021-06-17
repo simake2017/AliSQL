d91b62993c92db5a4f528410bf11a2970ca968de
wangy102 <31234273+simake2017@users.noreply.github.com>
2021-06-17 17:35:12 +0800

    1

--- cmake-build-debug/mysql-test/mysql-test-run.pl
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/perl
-# Call mtr in out-of-source build
-$ENV{MTR_BINDIR} = '/Users/wangyang/CLionProjects/mysql/AliSQL/cmake-build-debug';
-chdir('/Users/wangyang/CLionProjects/mysql/AliSQL/mysql-test');
-exit(system($^X, '/Users/wangyang/CLionProjects/mysql/AliSQL/mysql-test/mysql-test-run.pl', @ARGV) >> 8);
